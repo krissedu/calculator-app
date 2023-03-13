a807b784d60b0da98eea49c82ef44834046297d4
krissedu <nurdiansyahkrisnaputra@students.amikom.ac.id>
2023-03-13 17:04:45 +0700

    buat nyusahin yang kedua

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,14 @@ class Calculator
 {
 	static void main ( string[] args)
 	{
-
+	int a = 10;
+	int b = 6;
+	
+	Console.Writeline("Hasil penambahan : {0} + {1} = {2} ",a,b,penambahan (a,b));
+	Console.Writeline("Hasil pengurangan : {0} - {1} = {2} ",a,b,pengurangan (a,b));
+	
+	Console.Writeline("\n tekan sembarang key untuk keluar");
+	Console.Readkey();
 	}
 	
 	static int penambahan (int a ,int b)
